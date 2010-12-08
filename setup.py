f71c03361b87ef6e9ea9beb85a7dec9b2deba506
Grant Limberg <glimberg@cedrus.com>
2010-12-08 11:35:46 -0800

    updating version number to 1.0b4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pyxid",
-    version = "1.0b3",
+    version = "1.0b4",
     packages = find_packages(),
     install_requires = ["pyserial>=2.5"],
     author = "Grant Limberg",
