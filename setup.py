62143a07262b002e67398709b16645587a14bc6e
Grant Limberg <glimberg@cedrus.com>
2010-12-14 16:40:24 -0800

    fixed a typo in the setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pyxid",
-    version = "1.0b5",
+    version = "1.0b6",
     packages = find_packages(),
     install_requires = ["pyserial>=2.5"],
     author = "Grant Limberg",
@@ -10,7 +10,7 @@ setup(
     maintainer = "Cedrus Corporation",
     maintainer_email = "opensource@cedrus.com",
     description = ("Pure python library for communicating with Cedrus XID "
-                   "and StimTrackerdevices."),
+                   "and StimTracker devices."),
     long_description = open('README.txt').read(),
     license = "BSD",
     keywords = "cedrus xid XID stimulus response data collection",
