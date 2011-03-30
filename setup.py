7331b8e06c778bf8f949b6fa9aa6866c865e1773
Grant Limberg <glimberg@cedrus.com>
2011-03-30 10:49:12 -0700

    bump version number in setup.py to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "pyxid",
-    version = "1.0b6",
+    version = "1.0",
     packages = find_packages(),
     install_requires = ["pyserial>=2.5"],
     author = "Grant Limberg",
